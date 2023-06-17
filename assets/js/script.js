1016e2dff2cb88a8fbff1db3624ba3aa00123e36
ekrasutzky <ekrasutzky@gmail.com>
2023-06-17 22:16:59 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("connected");
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
